165b0dbcc4ae1fe395eb67442a5900fb32fa23d0
decay1232 <kaideng@umich.edu>
2024-04-30 10:10:42 -0400

    Algorithm_of_Choice

---
+++ script_Rosenbrock_2_Algorithm_of_Choice.m
@@ -0,0 +1,15 @@
+close all
+clear all
+clc
+
+addpath(genpath("functions"));
+problem.name = 'Rosenbrock_2';
+
+problem.x0 = ones(2, 1);
+problem.x0(1) = -1.2;
+
+%% (3) Method 3: Modified Newton with backtracking line search
+method.name = 'Newton';
+options = [];
+
+[x3,f3] = optSolver_JEKY(problem,method,options);
