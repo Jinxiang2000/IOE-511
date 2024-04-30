b7d38468cfeeb791051110d3d345017c9d18d8a0
ybshao0709 <ybshao@umich.edu>
2024-04-30 17:47:57 -0400

    new

---
+++ plot/optSolver_JEKY_analysis.m
@@ -0,0 +1,87 @@
+function [g_k_t] = optSolver_JEKY_analysis(problem,method,options)
+  % set problem, method and options
+  [problem] = setProblem(problem);
+  [method] = setMethod(method);
+  [options] = setOptions(options);
+
+  % compute initial function/gradient/Hessian
+  x = problem.x0; % starting point
+  f = problem.compute_f(x, problem); 
+  g = problem.compute_g(x, problem);
+  k = 0;
+  totalTime = 0; % This will accumulate the total running time
+
+  switch method.name
+      case {'GradientDescent', 'GradientDescentW'}
+          % No calculation of H is needed for Gradient Descent
+      case {'Newton', 'NewtonW'}
+          H = problem.compute_H(x, problem);
+      case {'TRNewtonCG', 'TRSR1CG'}
+          H = problem.compute_H(x, problem);
+      case {'BFGS', 'BFGSW'}
+          H = eye(length(x));
+      case {'L_BFGS', 'L_BFGSW'}
+          S = [];
+          Y = [];
+          rho = [];
+      %case {'TRNewtonCG', 'TRSR1CG'}
+
+      case {'DFP', 'DFPW'}
+          %D is the inverse Hessian Approximation
+          D = eye(length(x));
+      otherwise
+          error('Method not implemented yet!')
+  end
+
+  norm_g = norm(g,inf); % L-infty norm of gradient
+  norm_g_0 = norm_g;
+
+  g_k_t = [norm_g, k, totalTime]; % store the updated 'g-inf' after each iteration
+
+  while norm_g > options.term_tol * max(norm_g_0, 1) && k < options.max_iterations
+    tic; % Start timing this iteration
+
+    % take step according to a chosen method
+    switch method.name
+        case 'GradientDescent'
+            [x,f,g] = GradientDescent(x,g,problem,method);
+        case 'GradientDescentW'
+            [x,f,g] = GradientDescentW(x,g,problem,method);    
+        case 'Newton'
+            [x,f,g,H] = Newton(x,g,H,problem,method);
+        case 'NewtonW'
+            [x,f,g,H] = NewtonW(x,g,H,problem,method); 
+        case 'TRNewtonCG'
+            [x,f,g,H] = TRNewtonCG(x,g,H,problem,method);
+        case 'TRSR1CG'
+            [x,f,g,H] = TRSR1CG(x,g,H,problem,method,options);
+        case 'BFGS'
+            [x,f,g,H] = BFGS(x,g,H,problem,method,options);
+            %fprintf('Current function value: %.5f\n', f);
+        case 'BFGSW'
+            [x,f,g,H] = BFGSW(x,g,H,problem,method,options);   
+            %fprintf('Current function value: %.5f\n', f);
+        case 'DFP'
+            [x, f, g, D] = DFP(x, g, D, problem, method, options);
+        case 'DFPW'
+            [x, f, g, D] = DFPW(x, g, D, problem, method, options);   
+        case 'L_BFGS'
+            [x,f,g,S,Y,rho] = L_BFGS(x,g,S,Y,rho,problem,method,options);   
+            %fprintf('Current function value: %.5f\n', f);
+        case 'L_BFGSW'
+            [x,f,g,S,Y,rho] = L_BFGSW(x,g,S,Y,rho,problem,method,options);   
+            %fprintf('Current function value: %.5f\n', f);
+        otherwise
+            error('Method not implemented yet!')
+    end
+    norm_g = norm(g,inf);
+    k = k + 1;
+
+    elapsedTime = toc; % Measure time elapsed during this iteration
+    totalTime = totalTime + elapsedTime; % Accumulate total running time
+
+    g_k_t = [g_k_t; norm_g, k, totalTime]; 
+  end
+
+  %fprintf('Final #iterations: %.0f (%s) \n', k, method.name);
+end
---
+++ plot/plot_Rosenbrock100.m
@@ -0,0 +1,104 @@
+%% Rosenbrock
+close all
+clear all
+clc
+
+
+% set problem (minimal requirement: name of problem)
+currentDir = pwd;
+[parentDir, ~, ~] = fileparts(currentDir);
+functionsDir = fullfile(parentDir, 'functions');
+addpath(functionsDir);
+
+problem.name = 'Rosenbrock_100';
+problem.x0 = ones(100, 1);
+problem.x0(1) = -1.2;
+
+
+%% (1) Algorithm 1 (reference): BFGS
+method.name = 'BFGS';
+options = [];
+[g_k_t_1] = optSolver_JEKY_analysis(problem,method,options);
+%% (2.0) Algorithm 2.0: L-BFGS with m = 1
+method.name = 'L_BFGS';
+method.options.m = 1; 
+options = [];
+
+[g_k_t_20] = optSolver_JEKY_analysis(problem,method,options);
+%% (2.1) Algorithm 2.1: L-BFGS with m = 3
+method.name = 'L_BFGS';
+method.options.m = 3; 
+options = [];
+
+[g_k_t_21] = optSolver_JEKY_analysis(problem,method,options);
+
+%% (2.2) Algorithm 2.1: L-BFGS with m = 7
+method.name = 'L_BFGS';
+method.options.m = 7; 
+options = [];
+
+[g_k_t_22] = optSolver_JEKY_analysis(problem,method,options);
+%% (2.3) Algorithm 2.1: L-BFGS with m = 11
+method.name = 'L_BFGS';
+method.options.m = 11; 
+options = [];
+
+[g_k_t_23] = optSolver_JEKY_analysis(problem,method,options);
+%% (2.4) Algorithm 2.1: L-BFGS with m = 15
+method.name = 'L_BFGS';
+method.options.m = 15; 
+options = [];
+
+[g_k_t_24] = optSolver_JEKY_analysis(problem,method,options);
+%% (2.5) Algorithm 2.1: L-BFGS with m = 20
+method.name = 'L_BFGS';
+method.options.m = 20; 
+options = [];
+
+[g_k_t_25] = optSolver_JEKY_analysis(problem,method,options);
+
+
+%% (2.6) Algorithm 2.6: L-BFGS with m = 50
+method.name = 'L_BFGS';
+method.options.m = 50; 
+options = [];
+
+[g_k_t_26] = optSolver_JEKY_analysis(problem,method,options);
+
+%% Plotting ||g(x_k)||_{inf} vs k for each method
+figure; 
+% Plot each method with a different color
+semilogy(g_k_t_1(:,2), g_k_t_1(:,1), 'r-', 'LineWidth', 0.25); hold on;
+semilogy(g_k_t_21(:,2), g_k_t_21(:,1), 'b-', 'LineWidth', 0.25);
+semilogy(g_k_t_22(:,2), g_k_t_22(:,1), 'g-', 'LineWidth', 0.25);
+semilogy(g_k_t_23(:,2), g_k_t_23(:,1), 'k-', 'LineWidth', 0.25);
+semilogy(g_k_t_24(:,2), g_k_t_24(:,1), 'm-', 'LineWidth', 0.25);
+semilogy(g_k_t_25(:,2), g_k_t_25(:,1), 'c-', 'LineWidth', 0.25);
+semilogy(g_k_t_26(:,2), g_k_t_26(:,1), 'y-', 'LineWidth', 0.25);
+hold off;
+
+% Adding labels and legend
+xlabel('Iteration ($k$)', 'Interpreter', 'latex');
+ylabel('$||\nabla f(x_k)||_{inf}$', 'Interpreter', 'latex');
+legend('BFGS', 'L-BFGS (m = 3)', 'L-BFGS (m = 7)', 'L-BFGS (m = 11)', 'L-BFGS (m = 15)', 'L-BFGS (m = 20)', 'L-BFGS (m = 50)', 'Location', 'northeast');
+grid off;
+
+
+%% Plotting ||g(x_k)||_{inf} vs t for each method
+figure;
+
+% Plot each method with a different color
+semilogy(g_k_t_1(:,3), g_k_t_1(:,1), 'r-', 'LineWidth', 0.25); hold on;
+semilogy(g_k_t_21(:,3), g_k_t_21(:,1), 'b-', 'LineWidth', 0.25);
+semilogy(g_k_t_22(:,3), g_k_t_22(:,1), 'g-', 'LineWidth', 0.25);
+semilogy(g_k_t_23(:,3), g_k_t_23(:,1), 'k-', 'LineWidth', 0.25);
+semilogy(g_k_t_24(:,3), g_k_t_24(:,1), 'm-', 'LineWidth', 0.25);
+semilogy(g_k_t_25(:,3), g_k_t_25(:,1), 'c-', 'LineWidth', 0.25);
+semilogy(g_k_t_26(:,3), g_k_t_26(:,1), 'y-', 'LineWidth', 0.25);
+hold off;
+
+% Adding labels and legend
+xlabel('Time/Cost ($t$)', 'Interpreter', 'latex');
+ylabel('$||\nabla f(x_t)||_{inf}$', 'Interpreter', 'latex');
+legend('BFGS', 'L-BFGS (m = 3)', 'L-BFGS (m = 7)', 'L-BFGS (m = 11)', 'L-BFGS (m = 15)', 'L-BFGS (m = 20)', 'L-BFGS (m = 50)', 'Location', 'northeast');
+grid off;
